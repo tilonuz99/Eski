e85d7e8b2cb3b5323f4a0385466443e574e5502c
tilonuz <59096186+tilonuz99@users.noreply.github.com>
2020-06-11 15:57:38 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php 
+/**
+ *Author: Tilon
+ *
+ *Telegram : @TILON
+ */
+$API_KEY = 'token';
+##------------------------------##
+define('API_KEY',$API_KEY);
+function bot($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+ 
+ function sendaction($chat_id, $action){
+ bot('sendchataction',[
+ 'chat_id'=>$chat_id,
+ 'action'=>$action
+ ]);
+ }
+ //====================ᵗᶦᵏᵃᵖᵖ======================//
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$from_id = $message->from->id;
+$chat_id = $message->chat->id;
+$text = $message->text;
+//====================ᵗᶦᵏᵃᵖᵖ======================//
+if(preg_match('/^\/([Ss]tart)/',$text)){
+$start_time = round(microtime(true) * 1000);
+      $send=  bot('sendmessage', [
+                'chat_id' => $chat_id,
+                'text' =>"Tezlik:",
+            ])->result->message_id;
+        
+                    $end_time = round(microtime(true) * 1000);
+                    $time_taken = $end_time - $start_time;
+                    bot('editMessagetext',[
+                        "chat_id" => $chat_id,
+                        "message_id" => $send,
+                        "text" => "Tezlik:" . $time_taken . "ms",
+                    ]);
+}
+?>
